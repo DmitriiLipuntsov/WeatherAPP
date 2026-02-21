4b2f499654997ef1d4b40941f34f5d274a4266a5
DmitriiLipuntsov <55542391+DmitriiLipuntsov@users.noreply.github.com>
2026-02-21 18:06:42 +0500

    init: file structure

---
+++ WeatherAPP/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  WeatherAPP
+//
+//  Created by Dmitry Lipuntsov on 21.02.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+    }
+}
+
